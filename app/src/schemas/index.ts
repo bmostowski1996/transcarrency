1ca2d3498b2e20b5d69a5471e14b206f55665525
Toonz <johnnyt3.2014@gmail.com>
2025-05-22 10:28:44 -0400

    added index to schemas folder

--- app/src/schemas/index.ts
+++ app/src/schemas/index.ts
@@ -1,1 +1,6 @@
 // index for schemas to be used in the server
+import { typeDefs, resolvers } from './Schemas';
+
+export { typeDefs, resolvers };
+// This file serves as an index for all the schemas in the project.
+// It imports the type definitions and resolvers from the Schemas file and exports them.
