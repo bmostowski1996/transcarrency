445090a592753a24c637bd3be2e4652679495f3b
bmostowski1996 <bmostowski1996@gmail.com>
2025-05-29 22:03:09 -0400

    commit

--- app/src/controllers/UserControllers.ts
+++ app/src/controllers/UserControllers.ts
@@ -61,18 +61,18 @@
 //     }
 // };
 
-// export const loginUser = async (req: Request, res: Response) => {
-//     const {email, password} = req.body;
-//     try{
-//         const user = await User.findOne({email});
-//         if (!user || user.password !== password) {
-//             return res.status(401).json({message: 'Invalid email or password'});
-//         }
-//         res.status(200).json({message: 'Login successful', user});
-//     } catch (error) {
-//         res.status(500).json({message: 'Error logging in', error});
-//     }
-// };
+export const loginUser = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if (!user || user.password !== password) {
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+        res.status(200).json({message: 'Login successful', user});
+    } catch (error) {
+        res.status(500).json({message: 'Error logging in', error});
+    }
+};
 
 // export const logoutUser = async (req: Request, res: Response) => {
 //     try{
